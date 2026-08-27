17e6c0a9c3db7ac93af56c0952915aa6f653bb53
jackc <jfgcarson100@googlemail.com>
2026-08-27 15:26:52 +0100

    feat(player-controller): added movement speed increase & attenuation mechanics

--- src/player/scripts/player.gd
+++ src/player/scripts/player.gd
@@ -12,6 +12,8 @@ var camera_pitch : float = 0.0
 # Movement Variables
 @export var movement_speed : float
 @export var acceleration : float
+@export var sprint_multiplier : float
+@export var backward_speed_multiplier : float
 @export var air_movement_speed_percentage : float
 @export var jump_height : float
 
@@ -36,20 +38,32 @@ func _unhandled_input(event: InputEvent) -> void:
 func _physics_process(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-		
+
 	var input_vector := Input.get_vector("move_left", "move_right", "move_forward", "move_backwards")
 	var direction := (transform.basis * Vector3(input_vector.x, 0.0, input_vector.y)).normalized()
-	var control: float = 1.0 if is_on_floor() else air_movement_speed_percentage
+	var control : float = 1.0 if is_on_floor() else air_movement_speed_percentage
+
+	# How much the player is moving forwards.
+	var forwardness : float = (-input_vector.y + 1.0) / 2.0
+
+	# Interpolate between backward and forward speed values.
+	var speed_multiplier : float = lerp(backward_speed_multiplier, 1.0, forwardness)
+	
+	# Calculate player speed based on direction of movement and sprint status.
+	if Input.is_action_pressed("sprint") and forwardness > 0.5:
+		speed_multiplier = lerp(speed_multiplier, sprint_multiplier, forwardness)
+
+	var speed : float = movement_speed * speed_multiplier
 
 	if direction:
-		var target_velocity: Vector3 = direction * movement_speed
-		
-		velocity.x = move_toward(velocity.x, target_velocity.x,acceleration * control * delta)
+		var target_velocity : Vector3 = direction * speed
+
+		velocity.x = move_toward(velocity.x, target_velocity.x, acceleration * control * delta)
 		velocity.z = move_toward(velocity.z, target_velocity.z, acceleration * control * delta)
 	else:
 		velocity.x = move_toward(velocity.x, 0.0, acceleration * control * delta)
 		velocity.z = move_toward(velocity.z, 0.0, acceleration * control * delta)
-
+		
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = sqrt(2.0 * jump_height)
 
